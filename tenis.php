29c65fb0cd985c14fc2a3415a6eb45089f72bc32
mahdi1079 <mahdi.king.1079@gmail.com>
2024-11-15 10:38:05 -0800

    Update tenis.php

--- tenis.php
+++ tenis.php
@@ -1,7 +1,7 @@
 <?php
 include("header.html");
 ?>
-<section id="d2"><div id="f1"><img id="lebas2" src="4/7336510.jpg" alt=""><a id="lebas" href="tenis2.php">انواع وسایل تنیس</a></div></section>
+<section id="d2"><div id="f1"><img id="lebas2" src="https://s8.uupload.ir/files/7336510_ofl3.jpg" alt=""><a id="lebas" href="tenis2.php">انواع وسایل تنیس</a></div></section>
 <?php
 include("footer.html");
 ?>
