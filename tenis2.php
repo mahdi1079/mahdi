fdda159d9b7313292bb97c03681202c81e3daa21
mahdi1079 <mahdi.king.1079@gmail.com>
2024-11-15 10:40:17 -0800

    Update tenis2.php

--- tenis2.php
+++ tenis2.php
@@ -2,9 +2,9 @@
 include("header.html");
 ?>
 <section id="d2">
-    <div id="f1"><img id="lebas2" src="4/7336510.jpg "alt=""><a id="lebas" href="">راکت تنیس</a></div>
-    <div id="f1"><img id="lebas2" src="4/.5.jpg" alt=""><a id="lebas" href=""> میز تنیس</a></div>
-  n <div id="f1"><img id="lebas2" src="4/images (1).jpg" alt=""><a id="lebas" href="">توپ تنیس</a></div>
+    <div id="f1"><img id="lebas2" src="https://s8.uupload.ir/files/7336510_ofl3.jpg"alt=""><a id="lebas" href="">راکت تنیس</a></div>
+    <div id="f1"><img id="lebas2" src="https://s8.uupload.ir/files/.5_tcvu.jpg" alt=""><a id="lebas" href=""> میز تنیس</a></div>
+  n <div id="f1"><img id="lebas2" src="https://s8.uupload.ir/files/images_(1)_977r.jpg" alt=""><a id="lebas" href="">توپ تنیس</a></div>
 </section>
 </section>
 </section>
