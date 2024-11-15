dd4c1c12f851ae482ef2e168f691ff03c8dfcbec
mahdi1079 <mahdi.king.1079@gmail.com>
2024-11-15 10:30:51 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 include("header.html");
 ?>
 <section id="d2">
-<div id="f1"><img id="lebas2" src="4/images (1).jpg" alt=""><a id="lebas" href="">توپ تنیس</a></div>
-<div id="f1"><img id="lebas2" src="4/.5.jpg" alt=""><a id="lebas" href=""> میز تنیس</a></div>
-<div id="f1"><img id="lebas2"src="4/images.jpg" alt=""><a id="lebas" href="">ست لباس پرتغال</a></div>
-<div id="f1"><img id="lebas2" src="4/2.jpg" alt=""><a id="lebas" href="">ست لباس آرژانتین</a></div>
-<div id="f1"><img id="lebas2" src="4/7336510.jpg "alt=""><a id="lebas" href="">راکت تنیس</a></div>
-<div id="f1"><img id="lebas2" src="4/ed6fbf.webp" alt=""><a id="lebas" href="">انواع توپ</a></div>
+<div id="f1"><img id="lebas2" src="https://uupload.ir/view/ed6fbf_ffbo.webp/" alt=""><a id="lebas" href="">توپ تنیس</a></div>
+<div id="f1"><img id="lebas2" src="https://s8.uupload.ir/files/.5_l812.jpg" alt=""><a id="lebas" href=""> میز تنیس</a></div>
+<div id="f1"><img id="lebas2"src="https://s8.uupload.ir/files/images_fks1.jpg" alt=""><a id="lebas" href="">ست لباس پرتغال</a></div>
+<div id="f1"><img id="lebas2" src="https://s8.uupload.ir/files/2_8mrj.jpg" alt=""><a id="lebas" href="">ست لباس آرژانتین</a></div>
+<div id="f1"><img id="lebas2" src="https://s8.uupload.ir/files/7336510_l88h.jpg "alt=""><a id="lebas" href="">راکت تنیس</a></div>
+<div id="f1"><img id="lebas2" src="https://s8.uupload.ir/files/images_(1)_qzcc.jpg" alt=""><a id="lebas" href="">انواع توپ</a></div>
 </section>
 
 <?php
