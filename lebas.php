2193fc4b8c1c9ad9a1cea934644d87f630abf354
mahdi1079 <mahdi.king.1079@gmail.com>
2024-11-15 10:37:17 -0800

    Update lebas.php

--- lebas.php
+++ lebas.php
@@ -1,8 +1,8 @@
 <?php
 include("header.html");
 ?>
-<section id="d2"><div id="f1"><img id="lebas2"src="4/images.jpg" alt=""><a id="lebas" href="">ست لباس پرتغال</a></div>
-<div id="f1"><img id="lebas2" src="4/2.jpg" alt=""><a id="lebas" href="lebas.php">ست لباس آرژانتین</a></div></section>
+<section id="d2"><div id="f1"><img id="lebas2"src="https://s8.uupload.ir/files/images_1cdb.jpg" alt=""><a id="lebas" href="">ست لباس پرتغال</a></div>
+<div id="f1"><img id="lebas2" src="https://s8.uupload.ir/files/2_nqr.jpg" alt=""><a id="lebas" href="lebas.php">ست لباس آرژانتین</a></div></section>
 
 <?php
 include("footer.html");
