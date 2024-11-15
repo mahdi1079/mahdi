d40a723bbbd73ffa2a662c393aca2a975a727573
mahdi1079 <mahdi.king.1079@gmail.com>
2024-11-15 10:34:42 -0800

    Update fotbal.php

--- fotbal.php
+++ fotbal.php
@@ -2,8 +2,8 @@
 include("header.html");
 ?>
 <section id="d2">
-    <div id="f1"><img id="lebas2" src="4/2.jpg" alt=""><a id="lebas" href="lebas.php">ست انواع لباس ها</a></div>
-    <div id="f1"><img id="lebas2" src="4/ed6fbf.webp" alt=""><a id="lebas" href="">انواع توپ</a></div>
+    <div id="f1"><img id="lebas2" src="https://s8.uupload.ir/files/images_fks1.jpg" alt=""><a id="lebas" href="lebas.php">ست انواع لباس ها</a></div>
+    <div id="f1"><img id="lebas2" src="https://uupload.ir/view/ed6fbf_i1q1.webp/" alt=""><a id="lebas" href="">انواع توپ</a></div>
 </section>
 <?php
 include("footer.html");
