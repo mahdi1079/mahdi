a3ffbcc1cb1618f0acb819efb81a4e5d383eb097
mahdi1079 <mahdi.king.1079@gmail.com>
2024-11-21 23:11:06 -0800

    Delete tenis.php

--- tenis.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("header.html");
-?>
-<section id="d2"><div id="f1"><img id="lebas2" src="https://s8.uupload.ir/files/7336510_ofl3.jpg" alt=""><a id="lebas" href="tenis2.php">انواع وسایل تنیس</a></div></section>
-<?php
-include("footer.html");
-?>
-  
