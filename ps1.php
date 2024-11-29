88e8a4edce3a65a7ce695edee68897c13fa7a232
mahdi1079 <mahdi.king.1079@gmail.com>
2024-11-29 09:53:30 -0800

    Update ps1.php

--- ps1.php
+++ ps1.php
@@ -2,7 +2,7 @@
 include("header.html");
 ?>
 <section id="d2">
-<img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGQxN2l6dmptejUzaXpncHlyczhlM2J5bDZyZHZ3cHpjMmJ4bHR4diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/iDCeTPvQSPo7iKL5KR/giphy.gif" alt="">
+ <p>محتوایی وجود ندارد</p>
 </section>
 <?php
 include("footer.html");
