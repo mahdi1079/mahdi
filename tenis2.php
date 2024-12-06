86c6de9712bb63ab24c7c1e8d5037bb3a1123491
mahdi1079 <mahdi.king.1079@gmail.com>
2024-12-06 09:33:21 -0800

    Add files via upload

--- tenis2.php
+++ tenis2.php
@@ -3,7 +3,7 @@ include("header.html");
 ?>
 <section id="d2">
 <div id="f1"><a href="rtenis.php"><img id="lebas2" src="4/7336510.jpg "alt=""></a><a id="lebas" href="rtenis.php">راکت تنیس</a></div>
-<div id="f1"><a href="mtenis.php"><img id="lebas2" src="4/.5.jpg" alt=""></a><a id="lebas" href="mtenis.php"> میز تنیس</a></div>
+<div id="f1"><a href="mtenis.php"><img id="lebas2" src="4/vx4corgk_thumb3.webp" alt=""></a><a id="lebas" href="mtenis.php"> میز تنیس</a></div>
 <div id="f1"><a href="ttenis.php"><img id="lebas2" src="4/images (1).jpg" alt=""></a><a id="lebas" href="ttenis.php"> توپ تنیس</a></div>
 </section>
 </section>
